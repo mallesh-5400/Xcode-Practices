ccaa6aeb9d68dcf6e0dea31824ae4cecde99d191
MaadhavMohan <malleshahyalya@gmai.com>
2022-10-18 17:38:43 +0800

    Initial Commit

---
+++ Xcode-Practicess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xcode-Practicess
+//
+//  Created by user227716 on 10/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
