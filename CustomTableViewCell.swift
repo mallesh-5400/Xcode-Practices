4245461f63d711237ec2275b3156d695eed79515
MaadhavMohan <malleshahyalya@gmai.com>
2022-10-19 01:45:58 +0800

    Did Collapsing and Expanding of TableViewCell using setSelected func of UItableViewCell

--- CustomTableViewCell.swift
+++ CustomTableViewCell.swift
@@ -12,7 +12,7 @@ class CustomTableViewCell: UITableViewCell {
     lazy var imagVW:UIImageView = {
        let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.backgroundColor = .red
+        image.backgroundColor = UIColor(red: 45/255, green: 60/255, blue: 48/255, alpha: 1)
         image.heightAnchor.constraint(equalToConstant: 40).isActive = true
         image.widthAnchor.constraint(equalToConstant: 40).isActive = true
         return image
@@ -36,79 +36,94 @@ class CustomTableViewCell: UITableViewCell {
        let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.widthAnchor.constraint(equalToConstant: 80).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
         button.backgroundColor = .systemBlue
+       // button.titleLabel?.numberOfLines = 0
+        button.titleLabel?.contentCompressionResistancePriority(for: .horizontal)
+        button.setTitle("Continue", for: .normal)
         button.isHidden = true
         return button
     }()
-
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//       addViews()
-//       setUpUI()
-//    }
+    
+    lazy var mainStackView2: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [imagVW, innerStackView])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.distribution = UIStackView.Distribution.fill
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    lazy var innerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [title, subTitle])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = UIStackView.Alignment.fill
+        stackView.distribution = UIStackView.Distribution.fill
+        stackView.axis = NSLayoutConstraint.Axis.vertical
+        return stackView
+    }()
+    
+    lazy var mainStackView1: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [continueButton, mainStackView2])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.distribution = UIStackView.Distribution.fill
+        stackView.axis = NSLayoutConstraint.Axis.vertical
+        stackView.spacing = 5
+        stackView.backgroundColor = UIColor.cyan
+        return stackView
+    }()
+    
+    
+    
     
     private var buttonState: ContinueButtonState = .hidden
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addViews()
-        setUpUI()
+        setUpNewUI()
         
-        let gesture = UITapGestureRecognizer(target: self, action: #selector(buttonTap))
-        addGestureRecognizer(gesture)
+        //let gesture = UITapGestureRecognizer(target: self, action: #selector(buttonTap))
+       // addGestureRecognizer(gesture)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     func addViews() {
-        addSubview(imagVW)
-        addSubview(title)
-        addSubview(subTitle)
-        addSubview(continueButton)
+        addSubview(mainStackView1)
     }
-    func setUpUI() {
-        imagVW.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5).isActive = true
-        imagVW.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        imagVW.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
-       // imagVW.bottomAnchor
-       
-        
-        title.leadingAnchor.constraint(equalTo: trailingAnchor, constant: 50).isActive = true
-        title.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        title.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        title.heightAnchor.constraint(greaterThanOrEqualToConstant: 20).isActive = true
-        
-        subTitle.leadingAnchor.constraint(equalTo: title.leadingAnchor).isActive = true
-        subTitle.trailingAnchor.constraint(equalTo: title.trailingAnchor).isActive = true
-        subTitle.topAnchor.constraint(equalTo: title.bottomAnchor).isActive = true
-        //subTitle.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -5).isActive = true
-        
+    
+    func setUpNewUI() {
+        mainStackView1.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        mainStackView1.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        mainStackView1.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        mainStackView1.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
         hideButton()
-        
     }
     
     func unhideButton() {
         continueButton.isHidden = false
-        continueButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        continueButton.topAnchor.constraint(equalTo: subTitle.bottomAnchor, constant: 5).isActive = true
-        continueButton.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         buttonState = .unhidden
     }
     
     func hideButton() {
         continueButton.isHidden = true
-        continueButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = false
-        continueButton.topAnchor.constraint(equalTo: subTitle.bottomAnchor, constant: 5).isActive = false
-        continueButton.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = false
         buttonState = .hidden
     }
     
-   @objc func buttonTap() {
-       buttonState == .hidden ? unhideButton() : hideButton()
+    override func setSelected(_ selected: Bool, animated: Bool) {
+      //MARK: - If dont write below line also this function will behave we are expecting
+      //  super.setSelected(selected, animated: animated)
+        
+        if continueButton.isHidden, selected {
+            unhideButton()
+        } else {
+            hideButton()
+        }
     }
-    
 }
 
 enum ContinueButtonState {
